80d68235d341ec895bb7d46a3e48b17dd2701993
omzeton <adamr6561@gmail.com>
2019-02-16 13:12:14 +0100

    Study the passing of parameters between components - arrow function doesn't work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Person from './components/Person/Person';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    name: ''
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" onChange={this.nameChangeHandler.bind(this)} value={this.state.name}/>
+        <Person name={this.state.name} />
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+Why does arrow function not work?
+<input type="text" onChange={() => this.nameChangeHandler()}/>
+*/
---
+++ src/components/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = (props) => {
+	return (
+		<div>
+			<p>My name is {props.name}</p>
+		</div>
+	);
+};
+
+export default Person;
