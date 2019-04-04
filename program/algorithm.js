a0be9a75a02d62822f8fb459bad6a8ea3d04b46f
omzeton <adamr6561@gmail.com>
2019-04-04 02:08:46 +0200

    Reset repository to new

---
+++ program/algorithm.js
@@ -0,0 +1,67 @@
+/* ********************************* */
+
+// Remove m and t
+
+/* Infinitive To Perfect */
+
+if (verb == hollow) {
+	// Add 'ae'
+} else {
+	// Geminate 3rd radical
+	// Add 'ae' to it
+}
+
+/* ********************************* */
+/* Infinitive To Jussive / Imperative */
+
+if ( verb == verbClass.B || verb.length == 4 ) {
+	// Reduce 3rd radical's vowel to 0 or y
+} else if ( verb[0] == 'a' && verb == verbClass.A ) {
+	// reduce initial a to y;
+}
+
+if ( verb == hollow ) {
+	// reduce o to u
+	// reduce e to i
+}
+
+/* ********************************* */
+/* Infinitive To Gerund */
+
+if ( verb == hollow ) {
+	// vowel of 3rd radical 'a' to y
+} else {
+	// vowel of 3rd radical to y
+}
+
+if (verb.length == 3) {
+	if (verb[0] !== 'a') {
+		// 2nd radical give 'ae'
+	}
+	if (verb[2] == 'u') {
+		// if 2nd radical has 'u' change it to 'o'
+	}
+}
+
+if (verb[verb.length] == 'a') {
+	// verbs with final 'a' receive no 'ae'
+}
+
+/* ********************************* */
+/* Infinitive To Contingent */
+
+// vowel of 3rd radical reduce to 0 or 'y'
+
+if (verb[2] !== 'a') {
+	// 2nd radical gets 'ae'
+}
+
+if ( verb == verbClass.C && verb.length == 4 ) {
+	// geminate
+}
+
+if ( verb == hollow && 'with a' ) {
+	// reduce it to 'y'
+}
+
+/* ********************************* */
---
+++ program/testing-score.js
@@ -0,0 +1,29 @@
+				P			J			G			C		
+
+mefelleg		1			1			1			1
+
+memesker		1			1			1			1
+
+meselćet		1			1			1			1
+
+meqofer			1			0!			1			1
+
+mamen			1			1			1			1
+
+menkes			1			0!			1			1
+
+melak			1			1			1			1
+
+*memulat		1			1			0!			0!
+
+mewded			1			1*			1			1
+
+matat			1						1			1
+
+mehied			1						1			0!
+
+
+*SPRAWDZIĆ ODMIANĘ "MEMULAT"
+*Pamiętać o tym, że jak są obok siebie te same spółgłoski np ወድድ to usuwa się duplikat.
+
+// Nie zapomnieć o As, A, Te, Aste itp.
